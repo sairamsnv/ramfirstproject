99e8476b6bb5d1ce9c7ef099d843beb1dbf290f9
sairam <sayypureddysairam@gmail.com>
2019-09-30 14:15:57 +0530

    add new file

---
+++ ram.py
@@ -0,0 +1,11 @@
+n=input('enter a character')
+if n=="a":
+    print('Apple')
+elif n=="b":
+    print('Ball')
+elif n=="c":
+    print('Cat')
+elif n=="d":
+    print('Dog')
+else:
+    print('unknown')
